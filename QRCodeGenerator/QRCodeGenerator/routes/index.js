ef928d639b4cb858f2ea8975e7b635d139c1d3e0
Seok-IL <thirty950@naver.com>
2018-05-16 23:39:47 +0900

    QR코드를 생성하는 예제 프로그램 커밋

---
+++ QRCodeGenerator/QRCodeGenerator/routes/index.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+var qr = require('qr-image');
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/qrGen/:phoneNum', function (req, res) {
+    fs.writeFileSync(req.params.phoneNum + '.png', qr.imageSync("http://127.0.0.1:1337/qrShow/" + req.params.phoneNum));
+    res.render('index', { title: 'QR Code Generated!' });
+})
+
+router.get('/qrShow/:phoneNum', function (req, res) {
+    res.render('index', { title: 'QR Code Show : ' + req.params.phoneNum });
+})
+module.exports = router;
